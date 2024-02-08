0c9feeaed377cba6ce523d72d9ec59583269b940
Nick <nicky.kravtsov@gmail.com>
2024-02-08 09:32:59 +0200

    add config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,45 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            options.isDev ? "css-loader" : MiniCssExtractPlugin.loader,
+            "sass-loader",
+        ],
+    }
+
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const babelLoader = {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "babel-plugin-styled-components",
+                        {
+                            displayName: false
+                        }
+                    ],
+                ]
+            }
+        }
+    }
+
+    return [
+        babelLoader,
+        typescriptLoader,
+        cssLoader
+    ]
+}
